2efd162131c262e11d22b3296f0756b1a534954f
jmk976 <jmk976@gmail.com>
2021-04-13 15:53:47 +0900

    0413_sponsor

--- src/main/java/com/karma_and_tumblbug/p1/sponsor/SponsorDTO.java
+++ src/main/java/com/karma_and_tumblbug/p1/sponsor/SponsorDTO.java
@@ -1,5 +1,102 @@
 package com.karma_and_tumblbug.p1.sponsor;
 
+import java.sql.Date;
+
+import oracle.sql.DATE;
+
 public class SponsorDTO {
+	
+	private long sponNum;
+	private String sponSort;
+	private String payment;
+	private long money;
+	public long getSponNum() {
+		return sponNum;
+	}
+	public void setSponNum(long sponNum) {
+		this.sponNum = sponNum;
+	}
+	public String getSponSort() {
+		return sponSort;
+	}
+	public void setSponSort(String sponSort) {
+		this.sponSort = sponSort;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	public long getMoney() {
+		return money;
+	}
+	public void setMoney(long money) {
+		this.money = money;
+	}
+	public DATE getFirstPay() {
+		return firstPay;
+	}
+	public void setFirstPay(DATE firstPay) {
+		this.firstPay = firstPay;
+	}
+	public DATE getLastPay() {
+		return lastPay;
+	}
+	public void setLastPay(DATE lastPay) {
+		this.lastPay = lastPay;
+	}
+	public long getSponMonth() {
+		return sponMonth;
+	}
+	public void setSponMonth(long sponMonth) {
+		this.sponMonth = sponMonth;
+	}
+	public long getTotalMoney() {
+		return totalMoney;
+	}
+	public void setTotalMoney(long totalMoney) {
+		this.totalMoney = totalMoney;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	private DATE firstPay;
+	private DATE lastPay;
+	private long sponMonth;
+	private long totalMoney;
+	private String name;
+	private String phone;
+	private String email;
+	private String address;
+	private String id;
+	
 
 }
---
+++ src/main/webapp/WEB-INF/views/sponsor/sponsorSelect.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<div class="container">
+	<h1>${dto.name}님의 후원 현황 </h1>
+	<h3>후원분류 : ${dto.sponSort}</h3>
+	<h3>후원방법 : ${dto.payment}</h3>
+	<h3>후원금액 : ${dto.money}</h3>
+	
+	<c:if test="${dto.sponSort =='정기후원'}">
+		<h3>첫 결제일 : ${dto.firstPay}</h3>
+		<h3>최종 결제일 : ${dto.lastPay}</h3>
+		<h3>납부개월 수 : ${dto.sponMonth}</h3>
+		<h3>총 금액 : ${dto.totalMoney}</h3>
+	</c:if>
+		
+    <c:if test="${dto.sponSort =='일시후원'}">
+        <h3>결제일 : ${dto.firstPay}</h3> 
+    </c:if> 
+     
+		
+	
+</body>
+</html>
