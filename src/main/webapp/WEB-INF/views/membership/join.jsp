157ae056cb257913a1b28771b4113a1121628e19
Leepantam <nighfivego@gmail.com>
2021-04-15 16:15:04 +0900

    0415_membership join

--- src/main/webapp/WEB-INF/views/membership/join.jsp
+++ src/main/webapp/WEB-INF/views/membership/join.jsp
@@ -1,93 +1,86 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <c:import url="../template/bootStrap.jsp"></c:import>
+<style type="text/css">
+h4 {
+	color: red;
+}
+
+.b {
+	color: blue;
+}
+</style>
 </head>
 <body>
-<c:import url="../template/header.jsp"></c:import>
+	<c:import url="../template/header.jsp"></c:import>
 
-<div class="container">
+	<div class="container">
 		<h2>memberJoin</h2>
-		
-		<form action="./join" method="post" id="frm" enctype="multipart/form-data">
+
+		<form action="./join" method="post" id="frm"
+			enctype="multipart/form-data">
 			<div class="form-group">
 				<label for="id">ID</label> <input type="text" class="form-control"
 					id="id" name="id">
-					<!-- 
-					<a id="joinCheck" type="button" class="btn btn-outline-secondary">check</a>
-					 -->
 				<h4 id="idCheckResult"></h4>
-				<!-- 6글자 이상 -->
 			</div>
 			<div class="form-group">
 				<label for="pw">Password</label> <input type="password"
 					class="form-control" id="pw" name="pw">
-					<h4 id="pwResult"></h4>
-				<!-- 8글자 이상 -->
+				<h4 id="pwResult"></h4>
 			</div>
 			<div class="form-group">
-				<label for="pw">Password check</label> <input type="password"
+				<label for="pw2">Password check</label> <input type="password"
 					class="form-control" id="pw2" name="pw2">
-					<h4 id="pw2Result" ></h4>
-				<!-- pw 두개가 일치 -->
+				<h4 id="pw2Result"></h4>
 			</div>
 			<div class="form-group">
 				<label for="name">NAME</label> <input type="text"
-					class="form-control" id="name" name="name" class="etc">
-					<h4 id="nameResult" ></h4>
-				<!-- 비어있으면 안됨 -->
+					class="form-control" id="name" name="name">
+				<h4 id="nameResult"></h4>
 			</div>
 			<div class="form-group">
 				<label for="birth">BIRTH</label>
-				<table style="width:100%; height:38px;">
-				<thead></thead>
-				<tbody>
-				<tr>
-				<td>
-				<input type="text"
-					class="form-control" id="year" name="birth" class="etc">
-				</td>
-				<td>
-				<select style="width:150px; height:38px;" class="form-select" aria-label="Default select example" id="month" name="birth" class="etc">
-				<c:forEach begin="1" end="12" var="i">
-				<option>${i} 월</option>
-				</c:forEach>
-				</select>
-				</td>
-				<td>
-				<input type="text"
-					class="form-control" id="day" name="birth" class="etc">
-				</td>
-				</tr>
-				</tbody>
-				</table>
-				
-					<h4 id="birthResult" ></h4>
-				<!-- 비어있으면 안됨 -->
+
+				<div class="input-group mb-3" id="birth">
+
+					<input type="text" class="form-control" placeholder="년" id="year"
+						name="birth" class="birth" maxlength="4"> <select
+						style="width: 33%;" id="month" name="birth" class="birth">
+						<option>월</option>
+
+						<c:forEach begin="1" end="12" var="i">
+							<option>${i}월</option>
+						</c:forEach>
+
+					</select> <input type="text" class="form-control" placeholder="일" id="day"
+						name="birth" class="birth" maxlength="2">
+				</div>
+				<h4 id="birthResult"></h4>
 			</div>
 			<div class="form-group">
 				<label for="phone">PHONE</label> <input type="text"
 					class="form-control" id="phone" name="phone" class="etc">
-					<h4 id="phoneResult" ></h4>
-				<!-- 비어있으면 안됨 -->
+				<h4 id="phoneResult"></h4>
 			</div>
 			<div class="form-group">
 				<label for="address">ADDRESS</label> <input type="text"
 					class="form-control" id="address" name="address" class="etc">
-					<h4 id="pw2Result" ></h4>
-				<!-- 비어있으면 안됨 -->
+				<h4 id="addressResult"></h4>
 			</div>
-			
-			
-			<input type="submit" class="btn btn-primary" value="join" id="btn">
+
+
+			<a type="button" class="btn btn-primary" id="btn">JOIN</a>
 
 		</form>
 	</div>
-<script type="text/javascript" src="../resources/jquery/membershipJoin.js"></script>
+	<script type="text/javascript"
+		src="../resources/jquery/membershipJoin.js"></script>
 </body>
 </html>
--- src/main/webapp/resources/jquery/membershipJoin.js
+++ src/main/webapp/resources/jquery/membershipJoin.js
@@ -14,59 +14,264 @@ $("#joinCheck").click(function(){
 	
 })
  */
+
+let idCheck=false;
+let pwCheck=false;
+let nameCheck=false;
+let birthCheck=false;
+let phoneCheck=false;
+let addressCheck=false;
+
+
+
 $("#id").blur(function(){
 	let id=$("#id").val();
 	if(id!=""){
 		$.get("joinCheck?id="+id,function(result){
 			result = result.trim();
-			let str = "사용가능";
-			if(result=="불가"){
-				str = "사용 불가능";
+			let str = "사용 불가능";
+			let color = "r";
+			if(result=="가능"){
+				str = "사용가능";
+				color="b";
+				idCheck=true;
 			}
 			$("#idCheckResult").text(str);
+			$("#idCheckResult").attr("class",color);
 		})
-	
 	}else{	
-		$("#idCheckResult").text("ID 입력 요망");
-	
+		$("#idCheckResult").text("필수 정보 입니다.");
+		$("#idCheckResult").attr("class","r");
 	}
 });
 
 $("#id").focus(function(){
 	$("#idCheckResult").text("");
+	idCheck=false;
 })
 
 $("#pw").blur(function(){
 	let pw = $("#pw").val();
-	if(pw.length<8){
+	if(pw.length<1){
+		$("#pwResult").text("필수 정보 입니다.");
+	}else if(pw.length<8){
 		$("#pwResult").text("비밀번호는 8자 이상입니다.");
+	}else{
 	}
 })
 $("#pw").focus(function(){
 	$("#pwResult").text("");
 	$("#pw").val("");
+	$("#pw2").val("");
+	pwCheck=false;
 })
 
 $("#pw2").blur(function(){
 	let pw1=$("#pw").val();
 	let pw2=$("#pw2").val();
-		if(pw1=="" || pw1 != pw2){
-			$("#pw2Result").text("비밀번호 확인");
-		}else{
-			$("#pw2Result").text("");
-		}
+	if(pw1.length<8){
+		
+	}else if(pw2.length<1){
+		$("#pw2Result").text("필수 정보 입니다.");
+	}else if(pw1 != pw2){
+		$("#pw2Result").text("비밀번호 확인");
+	}else{
+		$("#pw2Result").text("");
+		pwCheck=true;
+	}
 })
+$("#pw2").focus(function(){
+	$("#pw2Result").text("");
+	$("#pw2").val("");
+	pwCheck=false;
+})
+
+
 
 $("#name").blur(function(){
 	let name = $("#name").val();
 	if(name==""){
-		$("#nameResult").text("필수 입력 사항입니다.");
+		$("#nameResult").text("필수 정보 입니다.");
 	}else{
 		$("#nameResult").text("");
+		nameCheck=true;
 	}
 })
-/*
-$("#pw2").focus(function(){
-	$("#pw2Result").text("");
+
+$("#name").focus(function(){
+	nameCheck=false;
 })
-*/
+
+
+
+$("#year").blur(function(){
+	let year = $("#year").val();
+	let month = $("#month").val();
+	let day = $("#day").val();
+	if(year.length<4){
+		$("#birthResult").text("태어난 년도 4자리를 정확하게 입력하세요");
+	} else if(month=="월"){
+		$("#birthResult").text("태어난 월을 선택하세요");
+	} else if(day ==""){
+		$("#birthResult").text("태어난 일(날짜) 2자리를 정확하게 입력하세요");
+	}else{
+		let lastDay=0;
+		switch($("#month").val()){
+			case "4월":
+			case "6월":
+			case "9월":
+			case "11월":
+				lastDay=30;
+				break;
+			case "2월":
+				if($("#year").val()%4==0){
+					lastDay=29;
+				}else{
+					lastDay=28;
+				}
+				break;
+			default:
+				lastDay=31;
+		}
+		if(year<1922 || year>2021){
+			$("#birthResult").text("년도 재확인");
+		} else if(day < 1 || day > lastDay ){
+			$("#birthResult").text("날짜 재확인");
+		}else{
+			
+		$("#birthResult").text("");
+		birthCheck=true;
+		}
+	}
+	
+})
+
+$("#month").blur(function(){
+	let year = $("#year").val();
+	let month = $("#month").val();
+	let day = $("#day").val();
+	if(year.length<4){
+		$("#birthResult").text("태어난 년도 4자리를 정확하게 입력하세요");
+	} else if(month=="월"){
+		$("#birthResult").text("태어난 월을 선택하세요");
+	} else if(day ==""){
+		$("#birthResult").text("태어난 일(날짜) 2자리를 정확하게 입력하세요");
+	}else{
+		let lastDay=0;
+		switch($("#month").val()){
+			case "4월":
+			case "6월":
+			case "9월":
+			case "11월":
+				lastDay=30;
+				break;
+			case "2월":
+				if($("#year").val()%4==0){
+					lastDay=29;
+				}else{
+					lastDay=28;
+				}
+				break;
+			default:
+				lastDay=31;
+		}
+		if(year<1922){
+			$("#birthResult").text("년도 재확인");
+		} else if(day < 1 || day > lastDay ){
+			$("#birthResult").text("날짜 재확인");
+		}else{
+			
+		$("#birthResult").text("");
+		birthCheck=true;
+		}
+	}
+	
+})
+
+$("#day").blur(function(){
+	let year = $("#year").val();
+	let month = $("#month").val();
+	let day = $("#day").val();
+	if(year.length<4){
+		$("#birthResult").text("태어난 년도 4자리를 정확하게 입력하세요");
+	} else if(month=="월"){
+		$("#birthResult").text("태어난 월을 선택하세요");
+	} else if(day ==""){
+		$("#birthResult").text("태어난 일(날짜) 2자리를 정확하게 입력하세요");
+	}else{
+		let lastDay=0;
+		switch($("#month").val()){
+			case "4월":
+			case "6월":
+			case "9월":
+			case "11월":
+				lastDay=30;
+				break;
+			case "2월":
+				if($("#year").val()%4==0){
+					lastDay=29;
+				}else{
+					lastDay=28;
+				}
+				break;
+			default:
+				lastDay=31;
+		}
+		if(year<1922){
+			$("#birthResult").text("년도 재확인");
+		} else if(day < 1 || day > lastDay ){
+			$("#birthResult").text("날짜 재확인");
+		}else{
+			
+		$("#birthResult").text("");
+		birthCheck=true;
+		}
+	}
+	
+})
+$("#year").focus(function(){
+	birthCheck=false;
+});
+$("#month").focus(function(){
+	birthCheck=false;
+});
+$("#day").focus(function(){
+	birthCheck=false;
+});
+
+$("#phone").blur(function(){
+	let phone = $("#phone").val();
+	if(phone==""){
+		$("#phoneResult").text("필수 정보 입니다.");
+	}else{
+		$("#phoneResult").text("");
+		phoneCheck=true;
+	}
+})
+
+$("#phone").focus(function(){
+	phoneCheck=false;
+})
+
+$("#address").blur(function(){
+	let address = $("#address").val();
+	if(address==""){
+		$("#addressResult").text("필수 정보 입니다.");
+	}else{
+		$("#addressResult").text("");
+		addressCheck=true;
+	}
+})
+$("#address").focus(function(){
+	addressCheck=false;
+})
+
+$("#btn").click(function(){
+	alert("id : "+idCheck);
+	alert("pw : "+pwCheck);
+	alert("name : "+nameCheck);
+	alert("birth : "+birthCheck);
+	alert("phone : "+phoneCheck);
+	alert("address : "+addressCheck);
+})
+
